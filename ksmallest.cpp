292c9a70ad98dab99c35dad72d38613df1c3d688
dikshajain228 <52117907+dikshajain228@users.noreply.github.com>
2019-08-28 21:15:38 +0530

    22/08/19

--- ksmallest.cpp
+++ ksmallest.cpp
@@ -15,13 +15,11 @@ int main()
 	for(i=0;i<k;i++){
 		min=i;
 		for(j=i+1;j<n;j++)
-		{
 			if(a[j]<a[min])
 				min=j;
-			int temp=a[i];
-			a[i]=a[min];
-			a[min]=temp;
-		}
+		int temp=a[i];
+		a[i]=a[min];
+		a[min]=temp;
 		
 	}
 	cout<<"kth smallest element is "<<a[k+1];
